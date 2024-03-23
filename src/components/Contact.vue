c64bde8f9e637e3caec1811cc43979c1e34b736a
August Wang <august.wang.tpe@gmail.com>
2024-03-23 11:56:25 +0800

    first

---
+++ src/components/Contact.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
+          <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">聯絡樹王生醫實業有限公司</h2>
+          <p class="mb-4 font-light">我們位於台北市天母西路132-9號，靠近台北榮總旁邊的米蘭小鎮一樓</p>
+          <p class="mb-4 font-medium">雖然裝潢不是高檔奢華，但是我們秉持著傳統古法，蒸餾出最純淨的精油提供給顧客使用，天然的精油不像精油香精一樣不能擦拭在皮膚上，透過基底油，可以調成按摩油按摩全身，處近血液循環，保持身體健康。
+            為了提供更好的服務品質，建議您可以先來電洽詢。
+          </p>
+          <a href="https://maps.app.goo.gl/56mt3DmuosdEvJeV7" target="_blank" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
+              跟我走
+              <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+          </a>
+      </div>
+  </div>
+</section>
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡老闆娘的Line</h2>
+            <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg" class="dark:hidden w-56 mx-auto" alt="qr code" />              
+            <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg" class="hidden dark:block w-56 mx-auto" alt="qr code" />              
+        </div>
+    </div>
+</section>
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡老闆娘Ｗechat</h2>
+            <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg" class="dark:hidden w-56 mx-auto" alt="qr code" />              
+            <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg" class="hidden dark:block w-56 mx-auto" alt="qr code" />              
+        </div>
+    </div>
+</section>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,80 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mt-7 mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">樹王生醫實業有限公司</h1>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl text-gray-500  dark:text-gray-400">大自然是醫生，請向大自然學習</h1>
+            <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">台灣森林寶藏，生命因大孜然而發光，樹王因大自然而榮耀</p>
+            <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">台北市天母西路132-9號 TEL: 0932-256-287</p>
+            <p class="max-w-2xl mb-13 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">Email: holy.tree@gmail.com</p>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mt-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                前往樹王
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+           
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="https://holytree.s3.us-west-2.amazonaws.com/logo.png" alt="mockup">
+        </div>                
+    </div>
+</section>
+
+
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158865.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157716.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157714.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158862.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157712.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158858.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158861.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157715.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157994.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157711.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157710.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158856.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+
+</template>
+
+<style scoped>
+
+</style>
