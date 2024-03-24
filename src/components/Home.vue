3d014e906f7fd4ea3fcd495b031337bdf6e56f6b
August Wang <august.wang.tpe@gmail.com>
2024-03-24 11:44:38 +0800

    change photo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,74 +3,107 @@
 </script>
 
 <template>
-<section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mt-7 mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">樹王生醫實業有限公司</h1>
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl text-gray-500  dark:text-gray-400">大自然是醫生，請向大自然學習</h1>
-            <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">台灣森林寶藏，生命因大孜然而發光，樹王因大自然而榮耀</p>
-            <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">台北市天母西路132-9號 TEL: 0932-256-287</p>
-            <p class="max-w-2xl mb-13 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">Email: holy.tree@gmail.com</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mt-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                前往樹王
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-           
+    <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1
+                    class="max-w-2xl mt-7 mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                    樹王生醫實業有限公司</h1>
+                <h1
+                    class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl text-gray-500  dark:text-gray-400">
+                    大自然是醫生，請向大自然學習</h1>
+                <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">
+                    台灣森林寶藏，生命因大孜然而發光，樹王因大自然而榮耀</p>
+                <p class="max-w-2xl mb-3 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">
+                    台北市天母西路132-9號 TEL: 0932-256-287</p>
+                <p class="max-w-2xl mb-13 font-light text-gray-500 lg:mb-3 md:text-lg lg:text-xl dark:text-gray-400">
+                    Email: holy.tree@gmail.com</p>
+                <a href="#"
+                    class="inline-flex items-center justify-center px-5 py-3 mt-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    前往樹王
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="https://holytree.s3.us-west-2.amazonaws.com/logo.png" alt="mockup">
+            </div>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://holytree.s3.us-west-2.amazonaws.com/logo.png" alt="mockup">
-        </div>                
-    </div>
-</section>
+    </section>
 
 
 
-<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158865.jpg" alt="">
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div class="grid gap-4">
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E6%AA%9C%E6%9C%A8%E4%B9%B3%E9%9C%9C.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E5%B0%88%E5%88%A9%E9%87%91%E7%89%8C%E7%8D%8E.jpg"
+                    alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E6%A8%9F%E6%A8%B9%E8%AD%B7%E5%BF%83.jpg" alt="">
+            </div>
         </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157716.jpg" alt="">
+        <div class="grid gap-4">
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E7%94%9F%E5%91%BD%E6%B3%89%E6%BA%90%E9%9C%B2%E4%B9%8B%E7%B2%BE%E9%9C%B2.jpg"
+                    alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E5%8B%95%E8%84%88%E7%A1%AC%E5%8C%96.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E6%A8%B9%E4%B9%8B%E7%B2%BE%E9%9C%B2.jpg" alt="">
+            </div>
         </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157714.jpg" alt="">
+        <div class="grid gap-4">
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E9%86%AB%E5%AD%B8%E5%90%8D%E4%BA%BA%E9%8C%84.jpg"
+                    alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E5%B0%88%E5%88%A9%E9%9B%86%E5%90%88.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E7%8D%A8%E7%85%A7%E9%85%8D%E9%85%92%E7%93%B6.jpg"
+                    alt="">
+            </div>
+        </div>
+
+        <div class="grid gap-4">
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E5%90%84%E7%A8%AE%E7%B2%BE%E6%B2%B9%E4%BB%8B%E7%B4%B9.jpg"
+                    alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E6%8A%91%E5%88%B6%E8%85%AB%E7%98%A4%E7%B4%B0%E8%83%9E%E7%B5%84%E5%90%88%E7%89%A9.jpg"
+                    alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg"
+                    src="https://holytree.s3.us-west-2.amazonaws.com/%E9%86%AB%E5%AD%B8%E5%90%8D%E4%BA%BA%E9%8C%84.jpg"
+                    alt="">
+            </div>
         </div>
     </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158862.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157712.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158858.jpg" alt="">
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158861.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157715.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157994.jpg" alt="">
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157711.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/157710.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://holytree.s3.us-west-2.amazonaws.com/158856.jpg" alt="">
-        </div>
-    </div>
-</div>
 
 
 </template>
