7f000c444632be3d94a9770f2c52be99768d0f34
August Wang <august.wang.tpe@gmail.com>
2024-03-24 11:23:05 +0800

    second finished page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,39 +3,58 @@
 </script>
 
 <template>
-<section class="bg-white dark:bg-gray-900">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-          <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">聯絡樹王生醫實業有限公司</h2>
-          <p class="mb-4 font-light">我們位於台北市天母西路132-9號，靠近台北榮總旁邊的米蘭小鎮一樓</p>
-          <p class="mb-4 font-medium">雖然裝潢不是高檔奢華，但是我們秉持著傳統古法，蒸餾出最純淨的精油提供給顧客使用，天然的精油不像精油香精一樣不能擦拭在皮膚上，透過基底油，可以調成按摩油按摩全身，處近血液循環，保持身體健康。
-            為了提供更好的服務品質，建議您可以先來電洽詢。
-          </p>
-          <a href="https://maps.app.goo.gl/56mt3DmuosdEvJeV7" target="_blank" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
-              跟我走
-              <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </a>
-      </div>
-  </div>
-</section>
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
-        <div class="mx-auto max-w-screen-sm text-center">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡老闆娘的Line</h2>
-            <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg" class="dark:hidden w-56 mx-auto" alt="qr code" />              
-            <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg" class="hidden dark:block w-56 mx-auto" alt="qr code" />              
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">聯絡樹王生醫實業有限公司</h2>
+                <p class="mb-4 font-light">我們位於台北市天母西路132-9號，靠近台北榮總旁邊的米蘭小鎮一樓</p>
+                <p class="mb-4 font-medium">
+                    雖然裝潢不是高檔奢華，但是我們秉持著傳統古法，蒸餾出最純淨的精油提供給顧客使用，天然的精油不像精油香精一樣不能擦拭在皮膚上，透過基底油，可以調成按摩油按摩全身，處近血液循環，保持身體健康。
+                    為了提供更好的服務品質，建議您可以先來電洽詢。
+                </p>
+                <a href="https://maps.app.goo.gl/56mt3DmuosdEvJeV7" target="_blank"
+                    class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
+                    跟我走
+                    <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.5012658184296!2d121.5219063!3d25.1187386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ae89a653aa69%3A0xb1c0886495f96099!2zMTEy5Y-w5YyX5biC5YyX5oqV5Y2A5aSp5q-N6KW_6LevMTMyLTnomZ8!5e0!3m2!1szh-TW!2stw!4v1711249503851!5m2!1szh-TW!2stw"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
+    </section>
+    <div class="flex p-3">
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+                <div class="mx-auto max-w-screen-sm text-center">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡Line
+                    </h2>
+                    <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg" class="dark:hidden w-56 mx-auto"
+                        alt="qr code" />
+                    <img src="https://holytree.s3.us-west-2.amazonaws.com/line.jpg"
+                        class="hidden dark:block w-56 mx-auto" alt="qr code" />
+                </div>
+            </div>
+        </section>
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
+                <div class="mx-auto max-w-screen-sm text-center">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡Ｗechat
+                    </h2>
+                    <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg" class="dark:hidden w-56 mx-auto"
+                        alt="qr code" />
+                    <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg"
+                        class="hidden dark:block w-56 mx-auto" alt="qr code" />
+                </div>
+            </div>
+        </section>
     </div>
-</section>
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 ">
-        <div class="mx-auto max-w-screen-sm text-center">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">聯絡老闆娘Ｗechat</h2>
-            <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg" class="dark:hidden w-56 mx-auto" alt="qr code" />              
-            <img src="https://holytree.s3.us-west-2.amazonaws.com/wechat.jpg" class="hidden dark:block w-56 mx-auto" alt="qr code" />              
-        </div>
-    </div>
-</section>
 
 </template>
 
